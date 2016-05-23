17bbae5952590742fd6453431a7c819b2a95b0ca
henryzzq <henryzhao@ZHAO-PC.us.oracle.com>
2016-05-23 14:44:01 +0800

    update script

--- ubuntu/btm/wls1036_observer/wlstartup.sh
+++ ubuntu/btm/wls1036_observer/wlstartup.sh
@@ -4,7 +4,7 @@ echo '**********************************************'
 echo BTMHOST=$BTM_HOST:$BTM_PORT
 echo '**********************************************'
 
-/root/Oracle/Middleware/user_projects/domains/base_domain/bin/setDomainEnv.sh
-/root/Oracle/Middleware/wlserver_10.3/nanoagent/bin/nanoEnvWeblogic.sh
+source /root/Oracle/Middleware/user_projects/domains/base_domain/bin/setDomainEnv.sh
+source /root/Oracle/Middleware/wlserver_10.3/nanoagent/bin/nanoEnvWeblogic.sh
 
 /root/Oracle/Middleware/user_projects/domains/base_domain/bin/startWebLogic.sh
